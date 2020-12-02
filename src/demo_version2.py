da0c7187b8d82a68c7b0906287cebfc19360a5c2
riyadgh <riyad.progs@gmail.com>
2020-12-02 12:23:51 -0500

    Rename file, and clean some unused code.

--- src/demo_version2.py
+++ src/demo_version2.py
@@ -1,11 +1,11 @@
 import requests
-import pprint
 from bs4 import BeautifulSoup
 
 
 def text_the_server():
     """
-    Text the server with a <color> to change the light, or 'previous' to get the previous color.
+    Text the server with a <color> to change the light's color, or 'previous' to get the previous color.
+    And can also text 'colors' to get a webpage link that contains the list of all colors.
     """
     phone_number = '5400000000'
     pphac_hue_light_number = '4848951386'
@@ -14,9 +14,8 @@ def text_the_server():
     response = requests.post('http://localhost:5000/', params=params)
     response.raise_for_status()
     message = response.text
-    soup = BeautifulSoup(message, 'html.parser') #'xml'
+    soup = BeautifulSoup(message, 'html.parser')
     print(soup.text)
-    # print(message)
 
 
 if __name__ == '__main__':
